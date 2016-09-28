7a2db789f077a7336bf58fb4d03134eb672fa855
mashype <mashype@me.com>
2016-09-27 21:42:06 -0700

    adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  devise_for :views
   resources :appointments
+
+
+  get 'welcome/index'
+ 
+  authenticated :user do
+  	root 'appointments#index', as: "authenticated_root"
+  end
+
+  root  'welcome#index'
 
 end
