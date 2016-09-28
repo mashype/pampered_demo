dad3cb3b8a0cde2b11fe922e456fccc71189869a
mashype <mashype@me.com>
2016-09-27 22:12:44 -0700

    adding heroku db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def index
-		set_meta_tags title: "Your private concierge",
-							keywords: "massage stylist hair nails",
-							description: "Fitness made social, get started today"
+
 	end
 
 end
