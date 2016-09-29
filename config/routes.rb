b615345c3247f7571266356a7d5d9711a93a03d2
mashype <mashype@me.com>
2016-09-29 14:54:20 -0700

    updated color logo

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 	devise_for :users, controllers: { registrations: "registrations" }
 
   get 'welcome/index'
+  get 'welcome/about' => 'welcome#about'
+  get 'welcome/faq' => 'welcome#faq'
+  get 'welcome/terms' => 'welcome#terms' 
+
  
   authenticated :user do
   	root 'appointments#index', as: "authenticated_root"
