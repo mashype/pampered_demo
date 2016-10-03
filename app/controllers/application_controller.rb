ad67e7f946e346d72ee43144d4e52b754b125083
mashype <mashype@me.com>
2016-10-03 12:17:58 -0700

    update meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :prepare_meta_tags, if: "request.get?"
+	before_action :set_coords
+
   rescue_from ActiveRecord::RecordNotFound do
     flash[:warning] = 'Resource not found.'
     redirect_back_or root_path
   end
-
-  before_action :set_coords
 
   def lookup_ip_location
 		Geocoder.search(request.remote_ip).first
@@ -19,5 +20,32 @@ class ApplicationController < ActionController::Base
 			session[:longitude] = coords.longitude
 		end
 	end	
+
+  def prepare_meta_tags(options={})
+    site_name   = "Pampered Up"
+    title       = [controller_name, action_name].join(" ")
+    description = "You don't have to be ridiculously rich to get a little pampering.  Bring private masseurs, chefs and stylists into your home for top notch treatment today!"
+    image       = options[:image] || "your-default-image-url"
+    current_url = request.url
+
+    # Let's prepare a nice set of defaults
+    defaults = {
+      site:        "Pampered Up",
+      title:       "You deserve it",
+      description: "You don't have to be ridiculously rich to get a little pampering.",
+      keywords:    "massage hair stylist private chef housekeeping pampering pamper",
+      twitter: {
+        site_name: "Pampered Up",
+        site: '@getyourpamp',
+        card: 'summary',
+        description: 'Pampered Twitter page',
+        image: image
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
+  end
   
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
 
 	def index
 
+    set_meta_tags   title: "Get Your Pamp",
+                    keywords: "massage hair stylist private chef housekeeping pampering pamper",
+                    description: "You don't have to be ridiculously rich to get a little pampering."
+
 	end
 
 end
