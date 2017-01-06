f179dfc1730fa9342777d30d5e649263fb37c7a6
mashype <mashype@me.com>
2017-01-06 14:27:24 -0800

    fixing small map

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,6 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
 
-  def aptsearch
-
-    @test_appointments = Appointment.test(params[:id])
-
-  end
-
 
   def aptfilter
 
