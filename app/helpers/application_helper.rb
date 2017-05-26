d6335f0ccd5550e1208630b2dd7857b04a650538
mashype <mashype@me.com>
2017-05-26 10:05:59 -0700

    membership cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
+	
 	def gravatar_for(user, size = 30, title = user.username)
-  	image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
+  		image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
 	end
+
+	def resource_name
+		:user
+	end
+
+	def resource
+		@resource ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
+
+
 end
+
+
+
